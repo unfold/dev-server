bd79c9dd64a977bcf36344aff01ee0c267d15389
Einar Löve <einar.love@gmail.com>
2015-04-14 20:15:18 +0200

    Dev-server can now be required as a module

--- bin/cli.js
+++ bin/cli.js
@@ -10,19 +10,13 @@ function resolve(file) {
   return path.resolve(file)
 }
 
-var defaults = {
-  port: process.env.PORT || 3000,
-  hostname: process.env.HOSTNAME || 'localhost',
-  config: resolve('webpack.config.js')
-}
-
 program
   .version(version, '-v --version')
   .description(description)
   .usage('app.js --index src/index.html --hostname myproject.dev --port 5000')
-  .option('-p, --port <n>', 'serve from port. Defaults to ' + defaults.port, defaults.port)
-  .option('--hostname <url>', 'serve from hostname. Defaults to ' + defaults.hostname, defaults.hostname)
-  .option('-c, --config <file>', 'your webpack config. Defaults to local webpack.config.js', resolve, defaults.config)
+  .option('-p, --port <n>', 'serve from port')
+  .option('--hostname <url>', 'serve from hostname')
+  .option('-c, --config <file>', 'your webpack config', require)
   .option('-m, --middleware <express app>', 'optional express server')
   .option('-i, --index <file>', 'optional file to return on request', resolve)
   .option('--log-browser-connections', 'log all browsers who connect to server')
