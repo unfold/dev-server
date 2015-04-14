288f6509950364a9a1f86a01fe842a5245cbaefc
Simen Brekken <simen@unfold.no>
2015-04-14 11:01:07 +0200

    Iniital import.

---
+++ bin/cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var minimist = require('minimist');
+var server = require('../lib');
+var path = require('path');
+
+var options = minimist(process.argv.slice(2), {
+  default: {
+    port: process.env.PORT || 3000,
+    hostname: process.env.HOSTNAME || '0.0.0.0',
+    config: path.resolve('webpack.config.js')
+  }
+});
+
+server(options);
---
+++ client/index.js
@@ -0,0 +1,37 @@
+var io = require('socket.io-client');
+var client = io.connect(__resourceQuery.substr(1));
+
+var initial = true;
+var currentHash = '';
+
+function reload() {
+  if (initial) {
+    initial = false;
+
+    return;
+  }
+
+  window.postMessage('webpackHotUpdate' + currentHash, '*');
+}
+
+client.on('hash', function(hash) {
+  currentHash = hash;
+});
+
+client.on('errors', function(errors) {
+  errors.forEach(function(error) {
+    console.error(error);
+  });
+
+  reload();
+});
+
+client.on('warnings', function(warnings) {
+  warnings.forEach(function(warning) {
+    console.warn(warning);
+  });
+
+  reload();
+});
+
+client.on('ok', reload);
