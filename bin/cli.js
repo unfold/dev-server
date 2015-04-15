e05ca8ee27eb59d30f9ad6f663da221b97d89b09
Einar Löve <einar.love@gmail.com>
2015-04-15 10:25:56 +0200

    Correctly resolve file paths when using CLI

--- bin/cli.js
+++ bin/cli.js
@@ -6,15 +6,24 @@ var path = require('path');
 var version = require('../package').version;
 var description = require('../package').description;
 
+// Commander sends undefined values to transform functions which bugs path.resolve
+function resolve(file) {
+  return path.resolve(file);
+}
+
+function resolveAndRequire(file) {
+  return require(resolve(file));
+}
+
 program
   .version(version, '-v --version')
   .description(description)
   .usage('app.js --index src/index.html --hostname myproject.dev --port 5000')
   .option('-p, --port <n>', 'serve from port')
   .option('--hostname <url>', 'serve from hostname')
-  .option('-c, --config <file>', 'your webpack config', require)
-  .option('-m, --middleware <express app>', 'optional express server')
-  .option('-i, --index <file>', 'optional file to return on request', function(file) { return path.resolve(file); })
+  .option('-c, --config <file>', 'your webpack config', resolveAndRequire)
+  .option('-m, --middleware <express app>', 'optional express server', resolveAndRequire)
+  .option('-i, --index <file>', 'optional file to return on request', resolve)
   .option('--log-browser-connections', 'log all browsers who connect to server')
   .parse(process.argv);
 
