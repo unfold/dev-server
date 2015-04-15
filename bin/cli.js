98b2d75aed8ae00d70cbbb01f5ddd9054cf23d1f
Simen Brekken <simen@unfold.no>
2015-04-15 15:54:19 +0200

    Add --env option.

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,7 @@
 
 var program = require('commander');
 var server = require('../server');
+var fs = require('fs');
 var path = require('path');
 var version = require('../package').version;
 var description = require('../package').description;
@@ -15,6 +16,19 @@ function resolveAndRequire(file) {
   return require(resolve(file));
 }
 
+function importEnv(file) {
+  fs.readFileSync(resolve(file), 'utf-8').split('\n').forEach(function(line) {
+    var matches = line.match(/(\w+)\s*=\s*(\S+)/);
+
+    if (matches) {
+      var key = matches[1];
+      var value = matches[2];
+
+      process.env[key] = process.env[key] || value;
+    }
+  });
+}
+
 program
   .version(version, '-v --version')
   .description(description)
@@ -24,7 +38,12 @@ program
   .option('-c, --config <file>', 'your webpack config', resolveAndRequire)
   .option('-m, --middleware <express app>', 'optional express server', resolveAndRequire)
   .option('-i, --index <file>', 'optional file to return on request', resolve)
+  .option('-e, --env [file]', 'import environment. defaults to .env')
   .option('--log-browser-connections', 'log all browsers who connect to server')
   .parse(process.argv);
+
+if (program.env) {
+  importEnv(typeof program.env === 'string' ? program.env : '.env');
+}
 
 server(program);
